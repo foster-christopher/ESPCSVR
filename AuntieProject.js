9a85aeef60e6f9c9648ec52e0bd23d45cecbaf50
Christopher Foster <christophervfoster@gmail.com>
2024-03-26 20:16:34 -0700

    Merged existing project files with fork of CSVReader pending name change

---
+++ AuntieProject.js
@@ -0,0 +1,37 @@
+//untitled project extending from Eclipse(?), searchable court reporter Steno app
+
+
+
+
+
+// takes data from Excel output of ESP (Existing steno program) and allows for searching via terms
+
+
+
+
+//possibly just using existing code from https://medium.com/@vivekvashistha/csv-reader-with-sort-and-filter-implementation-in-java-cb38869ab0b5
+
+// make it your own, good practice tool
+
+
+
+
+
+Custom data/CSV reader for StenoFastTrack using output of Eclipse program
+
+Currently untitled project extending from Eclipse(?), searchable court reporter Steno app
+
+
+
+
+
+takes data from Excel output of ESP (Existing steno program) and allows for searching via terms
+
+
+
+
+possibly just using existing code from https://medium.com/@vivekvashistha/csv-reader-with-sort-and-filter-implementation-in-java-cb38869ab0b5
+
+
+
+Two seperate views for Android and iOS devices
---
+++ Views/Android.js
@@ -0,0 +1,3 @@
+// Use XML search program for android platforms- 
+
+// https://developer.android.com/develop/ui/views/search/search-dialog
